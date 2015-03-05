317bda4a5a1a11f2cf3bb0e143f1c9f34473802c
Sebastien Chapuis <sebastien@chapuis.co>
2015-03-05 19:27:48 +0100

    ajout fichier header

---
+++ include/lemipc.h
@@ -0,0 +1,14 @@
+/*
+** lemipc.h for epitech in /home/chapuis_s/rendu/
+**
+** Made by chapui_s
+** Login   <chapui_s@epitech.eu>
+**
+** Started on  Thu Mar  5 19:27:16 2015 chapui_s
+** Last update Thu Mar  5 19:27:30 2015 chapui_s
+*/
+
+#ifndef LEMIPC_H_
+# define LEMIPC_H_
+
+#endif /* !LEMIPC_H_ */
